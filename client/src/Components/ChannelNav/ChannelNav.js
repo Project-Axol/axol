3c0a2e70235978002fbdaba949d4d9499e9bd550
Alec Prettyman <alecprettyman@alecs-mbp.lan>
2020-05-22 14:56:50 -0600

    structure update

---
+++ client/src/Components/ChannelNav/ChannelNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ChannelNav.css'
+
+function ChannelNav(){
+  return (
+    <section className='channel'>ChannelNav</section>
+  )
+}
+
+export default ChannelNav
