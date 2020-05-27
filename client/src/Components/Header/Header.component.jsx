8d923144a84b550612d539ef7177c318abd1176f
Bonsa-Tilahun <bonaatilahun@gmail.com>
2020-05-27 11:44:02 -0600

    added popup

--- client/src/Components/Header/Header.component.jsx
+++ client/src/Components/Header/Header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {connect} from 'react-redux'
 import {logoutUser} from '../../ducks/userReducer'
 import {withRouter, useLocation} from 'react-router-dom'
@@ -13,18 +13,53 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
 import './header.styels.scss'
+import PopUp from '../Popup/PopUp.component'
+import Axios from 'axios'
 
 const Header = (props) => {
     let location = useLocation()
-    
+    const [popUp, togglePopUp] = useState(false)
+    const [searchRes, setSearchRes]=useState([])
     const logout = () =>{
         auth.signOut().then(()=>{
             props.logoutUser()
             props.history.push('/')
         })
     }
+    const handleAddUserToChannel =(user)=>{
+        
+    }
+    const handleOnchange = (e) =>{
+        Axios.get(`/api/users?username=${e.target.value}`).then(res => {
+            setSearchRes(res.data)
+        }).catch(()=>alert('Something went wrong searching for user'))
+    }
+    const foundUsers = searchRes.map((user, i) => {
+        return (
+            <div key={i} onClick={()=>handleAddUserToChannel({user})}>
+                <div className='search-user-img'>
+                    <img src={user.profile_pic || `https://robohash.org/${user.user_id}`} alt="profile Picture"/>
+                </div>
+            </div>
+        )
+    })
     return(
         <div className='header-container'>
+            {popUp &&
+                <PopUp>
+                    <form onSubmit={handleAddUserToChannel}>
+                        <h1>Search Users</h1>
+                        <div className='crate-playlist-input-container'>
+                            <input placeholder='Username...' type="text" onChange={handleOnchange}/>
+                        </div>
+                        <div>
+                            {foundUsers}
+                        </div>
+                        <button onClick={()=>togglePopUp(!popUp)}>CANCEL</button>
+                        <button>CREATE PLAYLIST</button>
+                    </form>
+                </PopUp>
+            }
             <div className='header-home-icon'>
                 <IconButton 
                 className='header-logo'
@@ -43,7 +78,7 @@ const Header = (props) => {
                     </div>
                     <div className='header-server-channel'>
                         <Typography variant='h6'># Da Boiz</Typography>
-                        <img src={addPeople} alt="add users"/>
+                        <img src={addPeople} alt="add users" onClick={()=>togglePopUp(!popUp)}/>
                     </div>
                     <div className='header-sign-out' >
                         <div className='header-search-friends'>
