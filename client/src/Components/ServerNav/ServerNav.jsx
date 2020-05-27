375dec2f8c372fb0c5de9940561bfe6d85f9cdd2
mchalepage <me@mchale.page>
2020-05-27 15:55:08 -0600

    server nav changes

--- client/src/Components/ServerNav/ServerNav.jsx
+++ client/src/Components/ServerNav/ServerNav.jsx
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import './ServerNav.scss'
 import { selectServer } from '../../ducks/serverReducer'
+import IconButton from '@material-ui/core/IconButton'
+import findServerButton from '../../assets/icons8-compass-96.png'
 
 function ServerNav(props){
   const [servers, setServers] = useState([])
@@ -26,7 +28,9 @@ function ServerNav(props){
         onClick={() => {
           props.selectServer(server)
         }}
-      ></section>
+      >
+        <IconButton><div className='server-icon-btn'>Hi</div></IconButton>
+      </section>
     )
   })
 
@@ -36,8 +40,9 @@ function ServerNav(props){
         <div className='srvr-bttn'>Home</div>
     </Link>
       {serverDisplay}
-      <div className='srvr-bttn'>New</div>
-      <div className='srvr-bttn'>search</div>
+      <div className='server-nav-find-btn'>
+        <IconButton><img className='find-btn-img' src={findServerButton} alt='new-server-button'/></IconButton>
+      </div>
     </div>
   )
 }
