5302faa658a623bbefc4fb083cc21952017aa4c3
mchalepage <me@mchale.page>
2020-05-29 18:01:31 -0600

    friday afternoon changes

--- client/src/pages/ExplorePage/ExplorePage.jsx
+++ client/src/pages/ExplorePage/ExplorePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ServerNav from '../../Components/ServerNav/ServerNav'
 import Explore from '../../Components/Explore/Explore'
-import './ExplorePage.css'
+import './ExplorePage.scss'
 
 function ExplorePage(){
   return (
