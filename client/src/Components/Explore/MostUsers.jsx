acd3cae6cd5846f6c34452c7ca6cb40e46aaddb3
mchalepage <me@mchale.page>
2020-06-04 17:18:36 -0600

    adding server images

--- client/src/Components/Explore/MostUsers.jsx
+++ client/src/Components/Explore/MostUsers.jsx
@@ -5,6 +5,8 @@ import {selectServer} from '../../ducks/serverReducer'
 import { Paper, Typography } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
 
+import BlueAxolotl from '../../assets/blue-circle-icon.svg'
+
 
 function MostUsers(props) {
 const {server_name, server_img} = props.server
@@ -23,7 +25,7 @@ const {server_name, server_img} = props.server
         >
           <Grid container spacing={2} justify='center' alignItems='center' className='tile-grid-container' >
             <Grid item xs={12} className='grid-server-img'>
-              <img src={server_img} alt='server-img'/>
+              <img src={BlueAxolotl} alt='server-img'/>
             </Grid>
             <Grid item xs={12} className='grid-server-name'>
               {server_name}
